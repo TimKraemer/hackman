6211bbffdd74735a61c1ed287c49ccb354a03b83
Gregor Weber <gregorwbr@gmail.com>
2014-01-31 10:46:57 +0100

    hackfix

--- src/hacklevels.js
+++ src/hacklevels.js
@@ -2,10 +2,10 @@ levels = {
 	1: {
 		nodes: [
 			{x: 8, y: 7, chance: 8, time: 1000},
-			{x: 16, y: 7, chance: 2, time: 2000},
-			{x: 18, y: 9, chance: 1, type: 'chaser', time: 3000},
-			{x: 18, y: 5, chance: 3, time: 2500},
-			{x: 6, y: 9, chance: 4, time: 2500}
+			{x: 16, y: 7, chance: 2, time: 1000},
+			{x: 18, y: 9, chance: 1, type: 'chaser', time: 1000},
+			{x: 18, y: 5, chance: 3, time: 1000},
+			{x: 6, y: 9, chance: 4, time: 1000}
 		],
 		player: {x: 12, y: 7},
 		paths: [
